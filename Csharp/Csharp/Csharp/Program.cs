f34ceb7030c423ec7b98b5443899febf09d2e736
김수영 <rumex13@naver.com>
2017-04-03 15:33:49 +0900

    c#

--- Csharp/Csharp/Csharp/Program.cs
+++ Csharp/Csharp/Csharp/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Csharp
 {
-	class MainClass
+	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.Write("Hello");
+			Console.WriteLine("World..!");
+			// 나머지 연산의 부호는 좌항에 달려 있다
+			Console.WriteLine(5.0 % 2.2);
+			Console.WriteLine("가\\나\\다" + "라마바");
+			Console.WriteLine("안녕하세요"[0]);
+			Console.WriteLine("안녕");
+			Console.WriteLine('힣' - '가');
+			Console.WriteLine(true);
+			Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
 		}
 	}
 }
