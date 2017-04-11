da4ea7dd01d4b6402d5355d39ba680488f00149a
WinSU <rumex13@naver.com>
2017-04-11 13:58:06 +0900

    윈도우폼

---
+++ WindowForm/WindowForm/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.myButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // myButton
+            // 
+            this.myButton.Location = new System.Drawing.Point(13, 13);
+            this.myButton.Name = "myButton";
+            this.myButton.Size = new System.Drawing.Size(178, 23);
+            this.myButton.TabIndex = 0;
+            this.myButton.Text = "내마음대로변경";
+            this.myButton.UseVisualStyleBackColor = true;
+            this.myButton.Click += new System.EventHandler(this.myButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.myButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button myButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowForm/WindowForm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            myButton.Text = "코드에서 변경";
+            myButton.Width = 100;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Button button1 = new Button();
+                Controls.Add(button1);
+                button1.Location = new Point(13, 13 + (23 + 3) * (i+1));
+                button1.Text = "동적 "+ i +" 생성";
+            }
+             */
+            myButton.Click += Custom_Click;
+            FormClosed += Form1_FormClosed;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+            //MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            } while (result == DialogResult.Retry);
+
+
+        }
+    
+        private void Custom_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "@";
+            label1.Text += "@";
+
+            Button self = (Button)sender;
+            self.Text = "";
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+            label1.Text += "+";
+        }
+
+        private int elaspedTimer = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elaspedTimer++;
+            textBox1.Text = elaspedTimer%60 + "초 경과";
+            label1.Text = (elaspedTimer/60) + "분 경과";
+        }
+    }
+}
