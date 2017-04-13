6919515d4828ab3be978f798e6b1cdc932501f86
김수영 <rumex13@naver.com>
2017-04-13 17:42:01 +0900

    유니티 수업

---
+++ UnityProject/UnityClass/SimpleShooter/Assets/Scripts/CShipDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CShipDamage : MonoBehaviour {
+
+	// OnTriggerEnter2d()
+	// - IsTrigger 체크가 되어 있는 오브젝트가 충돌했을 경우 호출되는 이벤트 메소드
+	// collision 매개변수는 충돌한 상대 오브젝트의 콜라이더를 의미함
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log(other.name + "오브젝트와 충돌함");
+		// Destroy(게임오브젝트이름);
+		// Destroy(gameObject); // 비행기가 사라짐
+		Destroy(other.gameObject);
+	}
+
+}
