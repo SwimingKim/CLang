c13e20da136e951a1bd57e355a2a2b5ec0348c40
SU <rumex13@naver.com>
2017-05-09 16:13:19 +0900

    몬스터 스윙 공격

---
+++ UnityProject/PlatformerAdvance/Assets/Scripts/CBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CBullet : MonoBehaviour {
+
+    public float _dirValue; // 총알 발사 방향
+    public float _maxSpeed; // 총알 속도
+    protected Rigidbody2D _rigidbody2d; // 물리 엔진 컴포넌트 참조
+
+    public GameObject _destoyEffectPrefab; // 총알이 파괴될때 생성할 이펙트
+    public float _destoyEffectDestroyTime;
+
+	// 총알 초기화
+	public virtual void Init(bool isRightDir)
+	{
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+        _dirValue = (isRightDir) ? 1 : -1;
+    }
+
+    // 이동 메소드
+    public abstract void Move();
+
+	protected virtual void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			if (_destoyEffectPrefab != null)
+			{
+                GameObject effect = Instantiate(_destoyEffectPrefab, transform.position, Quaternion.identity);
+                Destroy(effect, _destoyEffectDestroyTime);
+            }
+			Destroy(gameObject);
+		}
+	}
+
+
+}
