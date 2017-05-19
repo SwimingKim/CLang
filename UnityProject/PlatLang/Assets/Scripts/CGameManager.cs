dae34f67ae1377c050db8c10c28ece0a38ef6128
SU <rumex13@naver.com>
2017-05-20 02:12:16 +0900

    2d프로젝트 UI

---
+++ UnityProject/PlatLang/Assets/Scripts/CGameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CGameManager : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public void LoadAttackStage()
+    {
+        SceneManager.LoadScene("Attack");
+    }
+
+    public void LoadSmashStage()
+    {
+        SceneManager.LoadScene("Smash");
+    }
+
+    public void LoadBlinkStage()
+    {
+        SceneManager.LoadScene("Blink");
+    }
+
+    public void LoadBombStage()
+    {
+        SceneManager.LoadScene("Bomb");
+    }
+
+    public void LoadMainStage()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
